795f0bb437f7ea902878d8790b819bff137fe9d5
by1991 <bduaa@connect.ust.hk>
2022-10-12 15:05:27 +0800

    Create compose_pipe.js

---
+++ compose_pipe.js
@@ -0,0 +1,17 @@
+// function composition of any number of functions
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x); 
+const double = x => x * 2
+const square = x => x * x
+
+// function composition
+var output_final = compose(square, double)(2);
+console.log(output_final);
+
+// function composition using pipe of any number of functions
+const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x); 
+const double2 = x => x * 2
+const square2 = x => x * x
+
+// function pipe
+var output_final = pipe(square2, double2)(2);
+console.log(output_final);
